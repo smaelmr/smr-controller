e272f50a97a82767f76648797d1375fd90db3967
Smael Munari <smunari@gmail.com>
2025-11-12 09:35:21 -0300

    ajuste url api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8088/api/v1',
+  baseURL: 'smr-api:8080/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
