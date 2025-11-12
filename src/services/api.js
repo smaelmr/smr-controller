cdf4b601007d119be34c71a852c8bba7c69550d2
Smael Munari <smunari@gmail.com>
2025-11-12 09:44:44 -0300

    ajuste protocolo conexão api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'smr-api:8080/api/v1',
+  baseURL: 'http://smr-api:8080/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
