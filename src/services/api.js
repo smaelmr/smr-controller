0728d828e4cd42030590f4750ec8d2d0e6624ee9
Smael Munari <smunari@gmail.com>
2025-11-12 10:10:46 -0300

    ajuste ip conexão api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://smr-api:8080/api/v1',
+  baseURL: 'http://31.97.168.79:8080/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
