7b6e1a7112a46e4f5883ed20d4b42bc3c21c3cd7
Smael Munari <smunari@gmail.com>
2025-12-03 11:12:43 -0300

    Ajuste configurações docker

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://31.97.168.79:8088/api/v1',
+  baseURL: 'http://31.97.168.79:8088/api/v2',
   headers: {
     'Content-Type': 'application/json',
   },
