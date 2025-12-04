55bad4ba2f659e1f03688d7d21621ad789352be0
Smael Munari da Rosa <smael.rosa@stone.com.br>
2025-12-03 23:17:50 -0300

    arquivo de config

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://31.97.168.79:8088/api/v1',
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://31.97.168.79:8088/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
